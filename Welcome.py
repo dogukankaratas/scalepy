7e0e5d7a9098425873e7d4ad69cfb5022a270079
Dogukan Karatas <karatasdogukan@gmail.com>
2023-01-19 14:42:29 +0100

    minor change

--- Welcome.py
+++ Welcome.py
@@ -18,7 +18,7 @@ st.markdown('### Step 1: Generate Response Spectrum')
 st.markdown("First generate a response spectrum with using built-in functions or upload a file.")
 col1, col2 = st.columns(2)
 with col1:
-    st.image('assets\\responseSpectrum.png')
+    st.image('assets/responseSpectrum.png')
 with col2:
     pass
 
@@ -26,25 +26,25 @@ st.markdown('### Step 2: Filter Record Database and Select Record Set')
 st.markdown('Filter your records acc. to the properties of your case and select an optimum set using ScalePy algorithm.')
 col3, col4 = st.columns(2)
 with col3:
-    st.image('assets\\filterRecords.png')
+    st.image('assets/filterRecords.png')
 with col4:
-    st.image('assets\\selectRecords.png')
+    st.image('assets/selectRecords.png')
     
 st.markdown('### Step 3: Amplitude Scaling')
 st.markdown('Perform amplitude scaling with selected spectral ordinate.')
 col5, col6 = st.columns(2)
 with col5:
-    st.image('assets\\scaleRecords.png')
+    st.image('assets/scaleRecords.png')
 with col6:
-    st.image('assets\\rotScale.png')
+    st.image('assets/rotScale.png')
     
 st.markdown('### Results')
 st.markdown('For every step, your data will be visualized in the chart.')
 col7, col8 = st.columns(2)
 with col7:
-    st.image('assets\\optimumRecords.png')
+    st.image('assets/optimumRecords.png')
 with col8:
-    st.image('assets\\scaledRecordsChart.png')
+    st.image('assets/scaledRecordsChart.png')
 
 st.markdown("Also you can read your scale factors and selected records from the table.")
-st.image('assets\\resultTable.png')
+st.image('assets/resultTable.png')
